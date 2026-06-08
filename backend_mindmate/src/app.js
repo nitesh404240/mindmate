d7615acf6e6ac2bca1ec2e333dde9e5bcbfc2565
nitesh404240 <nitesh.23bcon1309@jecrcu.edu.in>
2026-06-08 15:37:31 +0530

    adding the cors

--- backend_mindmate/src/app.js
+++ backend_mindmate/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:3000", credentials: true}));
+app.use(cors({origin: "https://mindmate-l8sb.vercel.app/", credentials: true}));
 app.use(cookieParser());
 app.use(
   express.json({
